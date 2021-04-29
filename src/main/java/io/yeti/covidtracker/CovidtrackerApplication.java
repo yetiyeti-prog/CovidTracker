c4e0dc80429dca5abc4aa69018278429045d22fd
Yetiyeti <sammy.karmarkar@gmail.com>
2021-04-30 00:29:36 +0530

    initial project setup commit 2

---
+++ src/main/java/io/yeti/covidtracker/CovidtrackerApplication.java
@@ -0,0 +1,13 @@
+package io.yeti.covidtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidtrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidtrackerApplication.class, args);
+	}
+
+}
